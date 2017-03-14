d5496c71f6320edc601abf62a89ffdce56526876
447093943@qq.com <zwu58@ucsc.edu>
2017-03-13 19:27:09 -0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {//注意这里是exports不是export
     ],//唯一入口文件，就像Java中的main方法
     output: {//输出目录
         path: __dirname + "/",//打包后的js文件存放的地方
+        publicPath:'https://benny201.github.io/PoTu/',
         filename: "bundle.js"//打包后的js文件名
     },
 
